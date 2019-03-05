b5a77e6283e12ec43a68f3278a3f3bcd00a397a7
Eduardo Paixao <42215395+EPEduardoPaixao@users.noreply.github.com>
2019-03-05 13:51:03 -0300

    Create Carousel.js

---
+++ src/componentes/Carousel.js
@@ -0,0 +1,26 @@
+// <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+    //   <ol className="carousel-indicators">
+    //     <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+    //     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    //     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    //   </ol>
+    //   <div className="carousel-inner">
+    //     <div className="carousel-item active">
+    //       <img className="d-block w-100" src="http://continentalshopping.com.br/wp-content/uploads/2017/07/2546_Continental_Site-Novo-Acontece_Banner_PETS-1.png" alt="Primeiro Slide"/>
+    //     </div>
+    //     <div className="carousel-item">
+    //       <img className="d-block w-100" src="https://archive.li/SbsBJ/15759a163e1a94f543ce605d119df942edbc2e6b.jpg" alt="Segundo Slide"/>
+    //     </div>
+    //     <div className="carousel-item">
+    //       <img className="d-block w-100" src="http://caiooliveira.com/wp-content/uploads/2016/12/castracaogatocachorro.jpg" alt="Terceiro Slide"/>
+    //     </div>
+    //   </div>
+    //   <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    //     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    //     <span className="sr-only">Anterior</span>
+    //   </a>
+    //   <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    //     <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    //     <span className="sr-only">Próximo</span>
+    //   </a>
+    // </div>
