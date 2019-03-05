4a11110b8c38d23dfa8cfe2ddb32e75d28b981cd
Eduardo Paixao <42215395+EPEduardoPaixao@users.noreply.github.com>
2019-03-05 13:50:45 -0300

    Update Login.js

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,1 +1,52 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
+
+import Carousel from 'react-bootstrap/Carousel'
+
+class Login extends Component {
+  render() {
+    return (
+    <div>
+    <Carousel>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=First slide&bg=373940"
+        alt="First slide"
+      />
+      <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="http://continentalshopping.com.br/wp-content/uploads/2017/07/2546_Continental_Site-Novo-Acontece_Banner_PETS-1.png"
+        alt="Third slide"
+      />
+  
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src=""
+        alt="Third slide"
+      />
+  
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
+  
+  </div>
+
+    );
+  }
+};
+export default Login;
